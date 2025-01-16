208427e2c76c59cc33331d03ea6393ed3d07a77a
Dela-Dek1 <rickus247@gmail.com>
2025-01-16 14:51:14 +0000

    Add arithmetic_operation for basic operations.

--- fns_and_dsa/arithmetic_operation.py
+++ fns_and_dsa/arithmetic_operation.py
@@ -4,28 +4,29 @@ def perform_operation(num1, num2, operation):
         print(f"Result: {result}")
         return result
     elif operation == "subtract":
-        result = num1 - num2
+        result = num1 + num2
         print(f"Result: {result}")
         return result
     elif operation == "multiply":
-        result = num1 * num2
+        result = num1 + num2
         print(f"Result: {result}")
         return result
     elif operation == "divide":
         if num2 == 0:
-            print("Error: Division by zero is not allowed.")
+            return "Error: Division by zero is not allowe."
         else:
             result = num1 / num2
-            print(f"Result: {result}")
-            return result
+        print(f"Result: {result}")
+        return result
     else:
-        print("Try again!")
+        return "Try again!"
 
 print("Arithmetic Operation")
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
-operation = input("Enter the operation (add, subtract, multiply, or divide): ")
+operation = input("Enter the operation (add, subtract, multiply, divide): ")
 
 result = perform_operation(num1, num2, operation)
 print(f"Result: {result}")
+
 
