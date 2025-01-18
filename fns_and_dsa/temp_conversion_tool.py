3c988826fd2def0c08b8c6b5e3c54f769177b804
Dela-Dek1 <rickus247@gmail.com>
2025-01-18 16:26:38 +0000

    Adding temp_conversion_tool.py to repo

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -5,10 +5,12 @@ CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 # conversion functions
 def convert_to_celsius(fahrenheit):
     """Convert Fahrenheit to Celsius."""
+    global FAHRENHEIT_TO_CELSIUS_FACOTR
     return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 def convert_to_fahrenheit(celsius):
     """Convert Celsius to Fahrenheit."""
+    global CELSIUS_TO_FAHRENHEIT_FACTOR
     return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
 # User Interaction
