4f493a00e96cb3a494d9aba50ce06b04914b7d30
Dela-Dek1 <rickus247@gmail.com>
2025-01-21 16:03:34 +0000

    Remove directory programming_paradigm

--- programming_paradigm/robust_division_calculator.py
+++
@@ -1,9 +0,0 @@
-def safe_divide(numerator, denominator):
-    try:
-        numerator = float(numerator)
-        denominator = float(denominator)
-        if denominator == 0:
-            return "Error: Cannot divide by zero."
-        return numerator / denominator
-    except ValueError:
-        return "Error: Please enter numeric values only."
